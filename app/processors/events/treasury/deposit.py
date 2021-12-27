e4a3f0637599b9917dcf4bac9aa2df7de16115c2
zzm <zzmwork8@gmail.com>
2021-12-27 15:51:16 +0800

    fix bug

--- app/processors/events/treasury/deposit.py
+++ app/processors/events/treasury/deposit.py
@@ -7,10 +7,10 @@ class TreasuryProposed(EventProcessor):
     event_id = 'Deposit'
 
     def process_search_index(self, db_session):
-        search_index = self.add_search_index(
-            index_type_id=SEARCH_INDEX_TREASURY_DEPOSIT,
-            account_id=SUBSTRATE_TREASURY_ACCOUNTS,
-            sorting_value=self.event.attributes[0]['value']
-        )
-
-        search_index.save(db_session)
+        for account_id in SUBSTRATE_TREASURY_ACCOUNTS:
+            search_index = self.add_search_index(
+                index_type_id=SEARCH_INDEX_TREASURY_DEPOSIT,
+                account_id=account_id,
+                sorting_value=self.event.attributes[0]['value']
+            )
+            search_index.save(db_session)
