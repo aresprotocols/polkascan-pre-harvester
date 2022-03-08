09013008c32392c883f12ab409a7f7965eb57680
zzm <zzmwork8@gmail.com>
2022-03-08 15:01:46 +0800

    handle BlockAlreadyAdded error

--- app/tasks.py
+++ app/tasks.py
@@ -132,7 +132,9 @@ def accumulate_block_recursive(self, block_hash, end_block_hash=None):
                 # Continue with parent block hash
                 block_hash = block.parent_hash
             except BlockAlreadyAdded as e:
+                block = Block.query(self.db_session).filter_by(hash=block_hash).first()
                 print('. Skipped {} '.format(block_hash))
+                block_hash = block.parent_hash
             except IntegrityError as e:
                 print('. Skipped duplicate {} '.format(block_hash))
             except Exception as exc:
