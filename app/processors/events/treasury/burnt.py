1a3f9d596cba9b36f7390cff697a31ca1617dd50
zzm <zzmwork8@gmail.com>
2022-03-14 17:16:34 +0800

    fix total_treasury_burn

--- app/processors/events/treasury/burnt.py
+++ app/processors/events/treasury/burnt.py
@@ -1,3 +1,4 @@
+from app.models.data import Event
 from app.processors import EventProcessor
 from app.settings import SEARCH_INDEX_TREASURY_AWARDED
 
@@ -9,6 +10,11 @@ class TreasuryBurnt(EventProcessor):
     def sequencing_hook(self, db_session, parent_block, parent_sequenced_block):
         if not parent_sequenced_block:
             parent_sequenced_block = {}
+        total_treasury_burn = parent_sequenced_block.get('total_treasury_burn', 0)
+        if total_treasury_burn == 0:
+            burns = db_session.query(Event).filter_by(module_id=self.module_id, event_id=self.event_id).all()
+            for _, burn in enumerate(burns):
+                value = burn.attributes[0]['value']
+                total_treasury_burn += value
         if self.sequenced_block:
-            self.sequenced_block.total_treasury_burn = int(parent_sequenced_block.get('total_treasury_burn', 0)) + \
-                                                       self.event.attributes[0]['value']
+            self.sequenced_block.total_treasury_burn = int(total_treasury_burn) + self.event.attributes[0]['value']
