c8fbea0f0a332b2874c6443b13490a1a08065fc6
zm Zhong <zzm.ftx@gmail.com>
2022-07-20 17:19:31 +0800

    adjust new version of estimates

--- app/processors/events/estimates/participate.py
+++ app/processors/events/estimates/participate.py
@@ -15,14 +15,18 @@ class ParticipateEstimates(EventProcessor):
             estimate_id = self.event.attributes[1]['value']
             participant = self.event.attributes[3]['value'].replace('0x', '')
             price = self.event.attributes[2]['value']['estimates']
+            option_index = self.event.attributes[2]['value']['range_index']
+            estimate_type = 'option' if option_index else 'price'
         else:
             raise ValueError('Event doensn\'t meet requirements')
 
         participant = EstimatesParticipants(
             symbol=symbol,
             estimate_id=estimate_id,
+            estimate_type=estimate_type,
             participant=participant,
             price=price,
+            option_index=option_index,
             created_at=self.event.block_id
         )
 
